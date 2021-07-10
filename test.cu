a965a09986d40c14f6d0ef1201bc83c1b86d5350
xdeep0 <77788695+xdeep0@users.noreply.github.com>
2021-07-11 08:18:25 +0900

    a

--- test.cu
+++ test.cu
@@ -6,7 +6,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__global__ void test(int *A, int *B, int n) {
+__global__ void test(char *A, char *B, int n) {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	if (index >= n) return;
     B[index] = A[index];
@@ -14,13 +14,13 @@ __global__ void test(int *A, int *B, int n) {
 
 int main () {
     int n = 5;
-	thrust::host_vector<int> h_A(n + 1);
-    thrust::host_vector<int> h_B(n + 1);
-	thrust::device_vector<int> d_A;
-    thrust::device_vector<int> d_B;
+	thrust::host_vector<char> h_A(n + 1);
+    thrust::host_vector<char> h_B(n + 1);
+	thrust::device_vector<char> d_A;
+    thrust::device_vector<char> d_B;
 
     for (int i = 0; i < n; i++) {
-        h_A[i] = i;
+        h_A[i] = '0' + i;
     }
 
 	d_A = h_A;
@@ -28,15 +28,15 @@ int main () {
 
     dim3 block(32, 1);
     dim3 grid((n + block.x - 1) / block.x, 1);
-	int *pd_A = thrust::raw_pointer_cast(&d_A[0]);
-	int *pd_B = thrust::raw_pointer_cast(&d_B[0]);
+	char *pd_A = thrust::raw_pointer_cast(&d_A[0]);
+	char *pd_B = thrust::raw_pointer_cast(&d_B[0]);
 
 	test<<< grid, block >>>(pd_A, pd_B, n);
 
     h_B = d_B;
 
     for (int i = 0; i < n; i++) {
-        printf("%d ", h_B[i]);
+        printf("%c ", h_B[i]);
     }
 
 }
