5c9b1fda7037869b1fda7f39b83b71ad919199ff
xdeep0 <77788695+xdeep0@users.noreply.github.com>
2021-07-11 10:14:12 +0900

    Update test.cu

--- test.cu
+++ test.cu
@@ -15,31 +15,34 @@ __global__ void test(char *A, char *B, int *C,int n) {
 
 int main () {
     int n = 12;
-	thrust::host_vector<char> h_A(n);
-	thrust::device_vector<char> d_A;
+    char A[] = "mississippi$";
+	thrust::host_vector<char> h_B(n);
+	thrust::device_vector<char> d_B;
 
     // char *T = (char *)malloc((n + 1) * sizeof(char));
     // for (int i = 0; i < n; i++) {
     //     T[i] = "mississippi$"[i];
     // }
-    char T[] = "mississippi$";
+    // char T[] = "mississippi$";
 
     // int SA_tmp[] = {11,10,7,4,1,0,9,8,6,3,5,2};
     // int *SA = (int *)malloc(n * sizeof(int));
     // for (int i = 0; i < n; i++) {
     //     SA[i] = SA_tmp[i];
     // }
-    int SA[] = {11,10,7,4,1,0,9,8,6,3,5,2};
+    // int SA[] = {11,10,7,4,1,0,9,8,6,3,5,2};
+    int C[] = {11,10,7,4,1,0,9,8,6,3,5,2};
 
-	d_A = h_A;
+	d_B = h_B;
 
-    dim3 block(32, 1);
+    dim3 block(8, 1);
     dim3 grid((n + block.x - 1) / block.x, 1);
-	char *pd_A = thrust::raw_pointer_cast(&d_A[0]);
+	char *pd_B = thrust::raw_pointer_cast(&d_A[0]);
 
-	test<<< grid, block >>>(T, pd_A, SA, n);
+	// test<<< grid, block >>>(T, pd_B, SA, n);
+    test<<< grid, block >>>(A, pd_B, C, n);
 
-    h_A = d_A;
+    h_B = d_B;
 
     for (int i = 0; i < n; i++) {
         printf("%c ", h_A[i]);
