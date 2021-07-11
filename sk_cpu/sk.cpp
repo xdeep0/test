dccd7422a5d33450d914a72b19532979f4f2b8ec
xdeep0 <77788695+xdeep0@users.noreply.github.com>
2021-07-11 09:11:30 +0900

    Update sk.cpp

--- sk_cpu/sk.cpp
+++ sk_cpu/sk.cpp
@@ -117,7 +117,7 @@ int main(int argc, char* argv[])
 	read_data(filename, data, n);				//read data set from the local file
 
 	// shin insert '$' ------------------
-	data[n - 1] = '.';
+	data[n - 1] = 'A';
 	// ----------------------------------
 
 	start = clock();							//record the start time
